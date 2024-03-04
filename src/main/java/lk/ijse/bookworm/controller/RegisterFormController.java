25520254833b6b9a10020d47b10b8786569f5759
Induwara <induwara2k@gmail.com>
2024-03-05 00:49:39 +0530

    add register Form

---
+++ src/main/java/lk/ijse/bookworm/controller/RegisterFormController.java
@@ -0,0 +1,35 @@
+package lk.ijse.bookworm.controller;
+
+import io.github.palexdev.materialfx.controls.MFXPasswordField;
+import io.github.palexdev.materialfx.controls.MFXTextField;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.AnchorPane;
+
+public class RegisterFormController {
+
+    @FXML
+    private AnchorPane registerPane;
+
+    @FXML
+    private MFXPasswordField txtConfirmPassword;
+
+    @FXML
+    private MFXPasswordField txtPassword;
+
+    @FXML
+    private MFXTextField txtUsername;
+
+    @FXML
+    void btnLogin(MouseEvent event) {
+
+    }
+
+    @FXML
+    void btnRegister(ActionEvent event) {
+
+    }
+
+}
+
