2a997d9849c0d74fe3c323e5e1f69bc08412eeaf
Induwara <induwara2k@gmail.com>
2024-03-05 17:20:09 +0530

    navigation done

--- src/main/java/lk/ijse/bookworm/controller/DashboardMainFormController.java
+++ src/main/java/lk/ijse/bookworm/controller/DashboardMainFormController.java
@@ -3,8 +3,11 @@ package lk.ijse.bookworm.controller;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.Pane;
+import javafx.stage.Stage;
 
 import java.io.IOException;
 
@@ -24,40 +27,68 @@ public class DashboardMainFormController {
 
 
     @FXML
-    void btnBookOnAction(ActionEvent event) {
-
+    void btnBookOnAction(ActionEvent event) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/view/bookForm.fxml"));
+        Pane bookPane = (Pane) fxmlLoader.load();
+        holderPane.getChildren().clear();
+        holderPane.getChildren().add(bookPane);
     }
 
     @FXML
-    void btnBorrowBookOnAction(ActionEvent event) {
-
+    void btnBorrowBookOnAction(ActionEvent event) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/view/borrowBookForm.fxml"));
+        Pane borrowBookPane = (Pane) fxmlLoader.load();
+        holderPane.getChildren().clear();
+        holderPane.getChildren().add(borrowBookPane);
     }
 
     @FXML
-    void btnBranchOnAction(ActionEvent event) {
-
+    void btnBranchOnAction(ActionEvent event) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/view/branchForm.fxml"));
+        Pane borrowBookPane = (Pane) fxmlLoader.load();
+        holderPane.getChildren().clear();
+        holderPane.getChildren().add(borrowBookPane);
     }
 
     @FXML
-    void btnDashboardOnAction(ActionEvent event) {
-
+    void btnDashboardOnAction(ActionEvent event) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/view/dashBoardForm.fxml"));
+        Pane dashboardPane = (Pane) fxmlLoader.load();
+        holderPane.getChildren().clear();
+        holderPane.getChildren().add(dashboardPane);
     }
 
     @FXML
-    void btnLogoutOnAction(ActionEvent event) {
+    void btnLogoutOnAction(ActionEvent event) throws IOException {
+        Parent rootNode = FXMLLoader.load(this.getClass().getResource("/view/mainForm.fxml"));
+        Scene scene = new Scene(rootNode);
+        Stage stage = new Stage();
+        stage.setScene(scene);
+        stage.centerOnScreen();
+        stage.setTitle("Login");
+        stage.show();
 
+        //Close the Current Window
+        Stage dashboard= (Stage) holderPane.getScene().getWindow();
+        dashboard.close();
     }
 
 
     @FXML
-    void btnSettingsOnAction(ActionEvent event) {
-
+    void btnSettingsOnAction(ActionEvent event) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/view/settingsForm.fxml"));
+        Pane settingPane = (Pane) fxmlLoader.load();
+        holderPane.getChildren().clear();
+        holderPane.getChildren().add(settingPane);
     }
 
 
     @FXML
-    void btnUserOnAction(ActionEvent event) {
-
+    void btnUserOnAction(ActionEvent event) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/view/userForm.fxml"));
+        Pane userPane = (Pane) fxmlLoader.load();
+        holderPane.getChildren().clear();
+        holderPane.getChildren().add(userPane);
     }
 
 
