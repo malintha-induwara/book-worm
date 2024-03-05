b00ee28df87f29da11f7001e7f37786fa7a89f58
Induwara <induwara2k@gmail.com>
2024-03-05 17:00:44 +0530

    btn style added

---
+++ src/main/java/lk/ijse/bookworm/controller/DashBoardFormController.java
@@ -0,0 +1,15 @@
+package lk.ijse.bookworm.controller;
+
+import javafx.fxml.FXML;
+import javafx.scene.layout.AnchorPane;
+
+public class DashBoardFormController {
+
+
+    @FXML
+    private AnchorPane dashBoardPane;
+
+
+
+}
+
--- src/main/java/lk/ijse/bookworm/controller/DashboardMainFormController.java
+++ src/main/java/lk/ijse/bookworm/controller/DashboardMainFormController.java
@@ -1,5 +1,65 @@
 package lk.ijse.bookworm.controller;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.Pane;
+
+import java.io.IOException;
+
 public class DashboardMainFormController {
+
+    @FXML
+    private AnchorPane holderPane;
+
+
+    public void initialize() throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/view/dashBoardForm.fxml"));
+        Pane dashboardPane = (Pane) fxmlLoader.load();
+        holderPane.getChildren().clear();
+        holderPane.getChildren().add(dashboardPane);
+    }
+
+
+
+    @FXML
+    void btnBookOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnBorrowBookOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnBranchOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnDashboardOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnLogoutOnAction(ActionEvent event) {
+
+    }
+
+
+    @FXML
+    void btnSettingsOnAction(ActionEvent event) {
+
+    }
+
+
+    @FXML
+    void btnUserOnAction(ActionEvent event) {
+
+    }
+
+
 }
 
