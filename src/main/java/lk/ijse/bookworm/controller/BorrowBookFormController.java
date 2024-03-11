434fbc8dd106fcc070f98161cabf093808e64983
Induwara <induwara2k@gmail.com>
2024-03-11 18:37:42 +0530

    borrow book ui and orrow book tm done

--- src/main/java/lk/ijse/bookworm/controller/BorrowBookFormController.java
+++ src/main/java/lk/ijse/bookworm/controller/BorrowBookFormController.java
@@ -1,6 +1,52 @@
 package lk.ijse.bookworm.controller;
 
+import io.github.palexdev.materialfx.controls.MFXComboBox;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.layout.AnchorPane;
+
 public class BorrowBookFormController {
+
+
+    @FXML
+    private AnchorPane borrowBookPane;
+
+    @FXML
+    private MFXComboBox<String> cmbBookID;
+
+    @FXML
+    private MFXComboBox<String> cmbUserID;
+
+    @FXML
+    private TableColumn<?, ?> colBookID;
+
+    @FXML
+    private TableColumn<?, ?> colBorrowDate;
+
+    @FXML
+    private TableColumn<?, ?> colID;
+
+    @FXML
+    private TableColumn<?, ?> colIsReturned;
+
+    @FXML
+    private TableColumn<?, ?> colRemove;
+
+    @FXML
+    private TableColumn<?, ?> colReturnDate;
+
+    @FXML
+    private TableColumn<?, ?> colUserID;
+
+    @FXML
+    private TableView<?> tblBranch;
+
+    @FXML
+    void btnAddOnAction(ActionEvent event) {
+
+    }
 
 
 
